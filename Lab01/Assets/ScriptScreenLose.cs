40c396974f9d2b531e83ee8844abebe18d66cbf7
Sergio Jiménez <sergiojj932@gmail.com>
2014-06-01 15:49:53 +0200

    Lab01

---
+++ Lab01/Assets/ScriptScreenLose.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptLo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
