05cbfe79cafbd9886d8cbe892aa4ebab8b1bc271
Sergio Jiménez <sergiojj932@gmail.com>
2014-06-01 22:54:44 +0200

    Lab01

---
+++ Lab01/Assets/ScriptEnemy.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptEnemy : MonoBehaviour {
+
+    public  int numberOfclicks = 2;
+    public float respawnWaitTime = 2.0F;
+    
+
+	
+
+	void Update () {
+        Move();
+	}
+
+    private void Move() {
+        if (numberOfclicks <= 0)
+        {
+            Debug.Log("Move");
+            var position = new Vector3(Random.Range(-6, 6), Random.Range(-4, 4), 0);
+            StartCoroutine(RespawnWaitTime());
+            transform.position = position;
+            numberOfclicks = 2;
+        }
+    }
+    private IEnumerator RespawnWaitTime(){
+        Debug.Log("RespawnWaitTime()");
+        renderer.enabled = false;
+        
+        yield return new WaitForSeconds(respawnWaitTime);
+        renderer.enabled = true;
+        
+    }
+
+
+    private  void WaitForSeconds() {
+        renderer.material.color = Color.red;
+    }
+
+    private void RandomColor() { 
+    }
+}
---
+++ Lab01/Assets/ScriptPlayer.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptPlayer : MonoBehaviour
+{
+    private const int LEFTCLICK = 0;
+    private const int RIGHTCLICK = 1;
+    private const int MIDDLECLICK = 2;
+
+    //this two must be public in order to allow the designer to change it's value from the editor
+    public const string targetTagName = "enemy";
+    public float rayDistance = 100;
+
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        GetMouseButton();
+    }
+
+    /// <summary>
+    /// Check in yo hit yuo hit the object that is in the position that you're pointing at
+    /// </summary>
+    private void checkIfClicked()
+    {
+
+        RaycastHit hit;
+        //A ray is an infinite line starting at origin and going in some direction.
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, rayDistance))
+        {
+            Relocate(hit.transform.tag.Equals("enemy"), hit);
+        }
+    }
+
+    /// <summary>
+    /// Realoate the enemys once the're hitted
+    /// </summary>
+    /// <param name="haveToAct"></param>
+    /// <param name="hit"></param>
+    private void Relocate(bool haveToAct, RaycastHit hit)
+    {
+        if (haveToAct)
+        {
+            ScriptEnemy enemyscript = hit.transform.GetComponent<ScriptEnemy>();
+            enemyscript.numberOfclicks -= 1;
+            Debug.Log("you hit an enemy.");
+        }
+        else
+        {
+            Debug.Log("This is not anenemy.");
+        }
+    }
+
+    /// <summary>
+    /// check if yhe player is clicking the right mouse button
+    /// </summary>
+    private void GetMouseButton()
+    {
+
+        if (Input.GetMouseButton(LEFTCLICK))
+        {
+
+            Debug.Log("Pressed left click.");
+            checkIfClicked();
+
+        }
+
+    }
+}
