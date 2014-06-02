e0fe8525aa6fc870abfe9d4beb22bda15ef69f1e
Sergio Jiménez <sergiojj932@gmail.com>
2014-06-02 12:27:52 +0200

    GUI

--- Lab01/Assets/ScriptMenu.cs
+++ Lab01/Assets/ScriptMenu.cs
@@ -12,4 +12,19 @@ public class ScriptMenu : MonoBehaviour {
 	void Update () {
 	
 	}
+
+    /// <summary>
+    /// Show what the user should see on the screen, this function is call by the Unity engine 
+    /// </summary>
+    void OnGUI()
+    {
+        if(GUI.Button(new Rect(10,10,90,50),"Start Game")){
+            Application.LoadLevel("sceneScreenLevel1");
+
+
+        }
+        if (GUI.Button(new Rect(10,70,90,50),"Exit game")) {
+            Application.Quit();
+        }
+    }
 }
