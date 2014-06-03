0274c8e51520381fa626b545fc7e647db5fd9f1d
Sergio Jiménez <sergiojj932@gmail.com>
2014-06-03 15:47:55 +0200

    player and bullet movements
    limits auto calculated

--- Lab02/Assets/ScriptPlayer.cs
+++ Lab02/Assets/ScriptPlayer.cs
@@ -7,75 +7,64 @@ public class ScriptPlayer : MonoBehaviour
     //inspector variables
     public float playerSpeedVertical = 10.0F;
     public float playerSpeedHorizontal = 10.0F;
+    public Transform proyectile;
+    public Transform socketProyectile;
 
+    //private variables
+    private Vector3 cameraWorldLimits;
 
     // Use this for initialization
     void Start()
     {
-
+        CalculateLimits();
     }
-
+    /// <summary>
+    /// Calculate the game limits, call on Startupbecause just need to calculate it once
+    /// </summary>
+    private void CalculateLimits()
+    {
+        //obtenemos el ancho alto y profunidad de lo que ve la camara en puntos del mundo
+        cameraWorldLimits = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Screen.dpi));
+        //obtenemos la caja que rodea la figura si es una esfera nos da el diametro y queremos el radio, lo mismo para un cubo
+        Mesh cubeMesh = GetComponent<MeshFilter>().mesh;
+        Vector3 objectSice = cubeMesh.bounds.size / 2;
+        cameraWorldLimits = new Vector3(cameraWorldLimits.x - objectSice.x, cameraWorldLimits.y - objectSice.y, cameraWorldLimits.z - objectSice.z);
+    }
 
     void Update()
     {
-
         Controller();
+        CreateBullet();
+    }
 
+    /// <summary>
+    /// Shot to an enemy when pressing down the space bar
+    /// </summary>
+    private void CreateBullet()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && proyectile!=null) {
+            Instantiate(proyectile,socketProyectile.position,socketProyectile.rotation);
+        }
     }
 
 
     private void Controller()
     {
-        /*
-            manera rudimentaria
-            if (Input.GetKey(KeyCode.UpArrow)) {
-                transform.Translate(0, playerSpeed * Time.deltaTime, 0);
-            }
-            if (Input.GetKey(KeyCode.DownArrow))
-            {
-                transform.Translate(0, -playerSpeed * Time.deltaTime, 0);
-            }
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                transform.Translate(-playerSpeed * Time.deltaTime, 0, 0);
-            }
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                transform.Translate(playerSpeed * Time.deltaTime, 0, 0);
-            }
-         */
-
+        //la velocidad de movimiento ira siempre en funcion de la velocidad del ordenador sobre
+        //el que se ejecuta el juego por eso usamos Time.deltaTime
         float transV = Input.GetAxis("Vertical")*playerSpeedVertical * Time.deltaTime;
         float transH= Input.GetAxis("Horizontal") * playerSpeedHorizontal * Time.deltaTime;
         transform.Translate(transH,transV,0);
         CheckLimits();
     }
-
+    /// <summary>
+    /// check the limits on a orthographic camera no matter wich resolution you use
+    /// </summary>
     private void CheckLimits()
     {
-        
-        float xlimit = 6.1F;
-        float ylimit = 4.4F;
-        /*manera rudimentaria
-        if (transform.position.x >= xlimit)
-        {
-            transform.position = new Vector3(xlimit, transform.position.y, transform.position.z);
-        }
-        if (transform.position.x <= -xlimit)
-        {
-            transform.position = new Vector3(-xlimit, transform.position.y, transform.position.z);
-        }
-        if (transform.position.y >= ylimit)
-        {
-            transform.position = new Vector3(transform.position.x, ylimit, transform.position.z);
-        }
-        if (transform.position.y <= -ylimit)
-        {
-            transform.position = new Vector3(transform.position.x, -ylimit, transform.position.z);
-        }*/
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x,-xlimit,xlimit), transform.position.y, transform.position.z);
-        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -ylimit, ylimit), transform.position.z);
-
+        //si llegas al limite, la posicion de la figura será igual al limite.
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -cameraWorldLimits.x, cameraWorldLimits.x), transform.position.y, transform.position.z);
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -cameraWorldLimits.y, cameraWorldLimits.y), transform.position.z);
 
     }
 }
--- Lab02/Assets/ScritBullet.cs
+++ Lab02/Assets/ScritBullet.cs
@@ -2,14 +2,46 @@
 using System.Collections;
 
 public class ScritBullet : MonoBehaviour {
+    //inspector ariables
+    public float bulletSpeed = 15.0F;
+
+
+    private Vector3 cameraWorldLimits;
 
 	// Use this for initialization
 	void Start () {
-	
+        CalculateLimits();
 	}
+
+    /// <summary>
+    /// Calculate the game limits, call on Startupbecause just need to calculate it once
+    /// </summary>
+    private void CalculateLimits() {
+        //obtenemos el ancho alto y profunidad de lo que ve la camara en puntos del mundo
+        cameraWorldLimits = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Screen.dpi));
+        //obtenemos la caja que rodea la figura si es una esfera nos da el diametro y queremos el radio, lo mismo para un cubo
+        Mesh cubeMesh = GetComponent<MeshFilter>().mesh;
+        Vector3 objectSice = cubeMesh.bounds.size / 2;
+        cameraWorldLimits = new Vector3(cameraWorldLimits.x - objectSice.x, cameraWorldLimits.y - objectSice.y, cameraWorldLimits.z - objectSice.z);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+        transform.Translate(0,bulletSpeed*Time.deltaTime,0);
+        DestroyBullet();
 	}
+
+
+
+    /// <summary>
+    /// check the limits on a orthographic camera no matter wich resolution you use
+    /// </summary>
+    private void DestroyBullet()
+    {        
+        //si llegas al limite, se destruye el objeto
+        if(transform.position.y > cameraWorldLimits.y){
+            Destroy(gameObject);
+        }
+
+    }
 }
