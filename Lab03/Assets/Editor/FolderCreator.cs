e48f54eb04c89de9c1b3f364aec41e89876c6897
Sergio Jiménez <sergiojj932@gmail.com>
2014-06-05 13:14:22 +0200

    little changes

---
+++ Lab03/Assets/Editor/FolderCreator.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// This class has the purpose of generate the folder inside our unity proyect automatically
+/// it's an Unity3D Editor script which is goint to extend the editor functionality
+/// </summary>
+public class FolderCreator {
+
+    /// <summary>
+    /// Create a new menu item at the top call Custom Tools 
+    /// and a submenu inside that one call Make Folders,it also provide
+    /// this submenu with functionality
+    /// </summary>
+    [MenuItem("Custom Tools/Make Folders")]
+    static void DoSomething()
+    {
+        MakeProject();
+    }
+
+    static void MakeProject()
+    {
+        Debug.Log("Doing Something...");
+    }
+}
