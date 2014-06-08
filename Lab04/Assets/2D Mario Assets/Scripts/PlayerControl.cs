2443860963d80ff48619e4144927b594bf847395
Sergio Jiménez <sergiojj932@gmail.com>
2014-06-08 15:46:09 +0200

    lab04 start

---
+++ Lab04/Assets/2D Mario Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerControl : MonoBehaviour
+{
+    public float walkSpeed = 1.5F;
+    public float runSpeed = 2.0F;
+    public float fallSpeed = 2.0F;
+    public float walkJump = 6.0F;
+    public float runJump = 9.0F;
+    public float crouchJump = 10.0F;
+    public float gravity = 20F;
+
+    private int moveDirection = 1;
+
+    private Vector3 velocity = Vector3.zero;
+
+
+    //private variables
+    private AnimationSprite animator;
+    private CharacterController marioControl;
+
+
+
+
+    void Update()
+    {
+        Controls();
+    }
+
+
+
+    private void Controls()
+    {
+        if (marioControl.isGrounded)
+        {
+            velocity = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+            ControlJump();
+        }
+        else
+        {
+            velocity.x = Input.GetAxis("Horizontal");
+            velocity.x *= walkSpeed;
+        }
+        velocity.y -= gravity * Time.deltaTime;
+        marioControl.Move(velocity * Time.deltaTime);
+    }
+
+    private void ControlJump()
+    {
+        //walk jump
+        if (Input.GetButtonDown("Jump") && Input.GetButton("Fire1"))
+        {
+            velocity.y = runJump;
+        }
+        //run jump
+        if (Input.GetButtonDown("Jump") && !Input.GetButton("Fire1"))
+        {
+            velocity.y = walkJump;
+        }
+    }
+
+
+    void Start()
+    {
+        animator = CreateAnimator();
+        marioControl = GetComponent<CharacterController>();
+    }
+
+    private AnimationSprite CreateAnimator()
+    {
+        int columnSize = 16;
+        int rowSize = 16;
+        int colFrameStart = 0;
+        int rowFrameStart = 1;
+        int totalFrames = 16;
+        float framesPerSecond = 12;
+        return new AnimationSprite(columnSize, rowSize, colFrameStart, rowFrameStart, totalFrames, framesPerSecond);
+
+    }
+}
