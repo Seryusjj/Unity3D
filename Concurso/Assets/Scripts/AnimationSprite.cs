741ee32c745c5c340842121a858a5e4527ee164e
Sergio Jiménez <sergiojj932@gmail.com>
2014-06-09 20:46:23 +0200

    koala animations

--- Concurso/Assets/Scripts/AnimationSprite.cs
+++ Concurso/Assets/Scripts/AnimationSprite.cs
@@ -2,9 +2,9 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class AnimationSprite 
+public class AnimationSprite
 {
-    
+
 
     private int columnSize;
     private int rowSize;
@@ -14,7 +14,8 @@ public class AnimationSprite
     private float framesPerSecond;
 
 
-    public  AnimationSprite(int columnSize, int rowSize, int colFrameStart, int rowFrameStart, int totalFrames, float framesPerSecond)
+
+    public AnimationSprite(int columnSize, int rowSize, int colFrameStart, int rowFrameStart, int totalFrames, float framesPerSecond)
     {
         this.columnSize = columnSize;
         this.rowSize = rowSize;
@@ -24,61 +25,73 @@ public class AnimationSprite
         this.framesPerSecond = framesPerSecond;
 
     }
+
     public void AnimateWithNormalMap(GameObject spriteObject, float gameTime)
     {
-        int index = (int)(gameTime * framesPerSecond);                      //time control fpd
+        int index = (int)(gameTime * framesPerSecond);
         index = index % totalFrames;
 
-        Vector2 size = new Vector2(1.0F / columnSize, 1.0F / rowSize);          // scale
+        Vector2 size = new Vector2(1.0F / columnSize, 1.0F / rowSize);
 
         int u = index % columnSize;
         int v = index / columnSize;
 
 
-        Vector2 offset = new Vector2((u + colFrameStart) * size.x, (1 - size.y) - ((v + rowFrameStart) * size.y));              //offset
+        Vector2 offset = new Vector2((u + colFrameStart) * size.x, (1 - size.y) - ((v + rowFrameStart) * size.y));
 
-        spriteObject.renderer.material.mainTextureOffset = offset;                   //texture offset
-        spriteObject.renderer.material.mainTextureScale = size;                      //texture scale
+        spriteObject.renderer.material.mainTextureOffset = offset;
+        spriteObject.renderer.material.mainTextureScale = size;
 
         spriteObject.renderer.material.SetTextureOffset("_BumpMap", offset);
         spriteObject.renderer.material.SetTextureScale("_BumpMap", size);
     }
 
-    public void Animate(GameObject spriteObject,float gameTime)
+    public void Animate(GameObject spriteObject, float gameTime)
     {
-        int index = (int)(gameTime * framesPerSecond);                //time control fpd
+        int index = (int)(gameTime * framesPerSecond);
         index = index % totalFrames;
 
-        Vector2 size = new Vector2(1.0F / columnSize, 1.0F / rowSize);          // scale
+        Vector2 size = new Vector2(1.0F / columnSize, 1.0F / rowSize);
 
         int u = index % columnSize;
         int v = index / columnSize;
 
-        Vector2 offset = new Vector2((u + colFrameStart) * size.x, (1 - size.y) - ((v + rowFrameStart) * size.y));              //offset
+        Vector2 offset = new Vector2((u + colFrameStart) * size.x, (1 - size.y) - ((v + rowFrameStart) * size.y));
 
-        spriteObject.renderer.material.mainTextureOffset = offset;                   //texture offset
-        spriteObject.renderer.material.mainTextureScale = size;                      //texture scale
+        spriteObject.renderer.material.mainTextureOffset = offset;
+        spriteObject.renderer.material.mainTextureScale = size;
     }
 
-    public void AnimateFont(GameObject spriteObject,float gameTime, string type)
+
+
+
+
+
+
+
+
+
+
+
+    public void AnimateFont(GameObject spriteObject, float gameTime, string type)
     {
         int index = CalculateFontIndex(gameTime, type);
 
-        Vector2 size = new Vector2(1.0F / columnSize, 1.0F / rowSize);          // scale
+        Vector2 size = new Vector2(1.0F / columnSize, 1.0F / rowSize);
 
         int u = index % columnSize;
         int v = index / columnSize;
 
-        Vector2 offset = new Vector2((u + colFrameStart) * size.x, (1 - size.y) - ((v + rowFrameStart) * size.y));              //offset
+        Vector2 offset = new Vector2((u + colFrameStart) * size.x, (1 - size.y) - ((v + rowFrameStart) * size.y));
 
-        spriteObject.renderer.material.mainTextureOffset = offset;                   //texture offset
-        spriteObject.renderer.material.mainTextureScale = size;                      //texture scale
+        spriteObject.renderer.material.mainTextureOffset = offset;
+        spriteObject.renderer.material.mainTextureScale = size;
     }
 
     private int CalculateFontIndex(float gameTime, string type)
     {
-        int index = (int)(gameTime);                //time control fpd
-        
+        int index = (int)(gameTime);
+
 
         var font1 = index % 10;
         var font2 = ((index - font1) / 10) % 10;
@@ -90,6 +103,47 @@ public class AnimationSprite
         else if (type.Equals("font3")) index = font3;
         else if (type.Equals("font4")) index = font4;
 
-        return index;        
+        return index;
     }
+
+
+    public AnimationSprite SetColumnSize(int columnSize)
+    {
+        this.columnSize = columnSize;
+        return this;
+    }
+    public AnimationSprite SetRowSize(int rowSize)
+    {
+        this.rowSize = rowSize;
+        return this;
+    }
+
+    public AnimationSprite SetTotalFrames(int totalFrames)
+    {
+        this.totalFrames = totalFrames;
+        return this;
+    }
+
+    public AnimationSprite SetFramesPerSecond(int framesPerSecond)
+    {
+        this.framesPerSecond = framesPerSecond;
+        return this;
+    }
+
+    public AnimationSprite SetRowFrameStart(int rowFrameStart)
+    {
+        this.rowFrameStart = rowFrameStart;
+        return this;
+    }
+
+
+    public AnimationSprite SetColFrameStart(int colFrameStart)
+    {
+        this.colFrameStart = colFrameStart;
+        return this;
+    }
+
+
+
+
 }
--- Concurso/Assets/Scripts/KoalaBehaviour.cs
+++ Concurso/Assets/Scripts/KoalaBehaviour.cs
@@ -5,6 +5,9 @@ public class KoalaBehaviour : MonoBehaviour
 {
 
     private AnimationSprite animator;
+    public enum Direction { Right, Left };
+    public Direction currentDirection = Direction.Right;
+
     void Start()
     {
         animator = CreateAnimator();
@@ -13,18 +16,82 @@ public class KoalaBehaviour : MonoBehaviour
 
     private AnimationSprite CreateAnimator()
     {
-        int columnSize = 5;
-        int rowSize = 1;
-        int colFrameStart = 1;
+        int columnSize = 8;
+        int rowSize = 2;
+        int colFrameStart = 2;
         int rowFrameStart = 0;
-        int totalFrames=4;
-        float framesPerSecond= 4;
+        int totalFrames=6;
+        float framesPerSecond= 5;
         return new AnimationSprite(columnSize, rowSize, colFrameStart, rowFrameStart, totalFrames, framesPerSecond);
     }
 
     // Update is called once per frame
     void Update()
     {
-        animator.Animate(gameObject, Time.time);
+        Controls();
+        //IdleAnimation();
+        WalkAnimation();
+        //JumpAnimation();
+    }
+
+
+
+
+    private void IdleAnimation()
+    {
+        if (currentDirection == Direction.Right)
+        {
+
+            animator.SetColFrameStart(0).SetRowFrameStart(0).SetTotalFrames(2).SetFramesPerSecond(2);
+            animator.Animate(gameObject, Time.time);
+        }
+        else if (currentDirection == Direction.Left)
+        {
+            animator.SetColFrameStart(0).SetRowFrameStart(1).SetTotalFrames(2).SetFramesPerSecond(2);
+            animator.Animate(gameObject, Time.time);
+        }
+    }
+
+    private void WalkAnimation()
+    {
+        if (currentDirection == Direction.Right)
+        {
+
+            animator.SetColFrameStart(2).SetRowFrameStart(0).SetTotalFrames(6).SetFramesPerSecond(5);
+            animator.Animate(gameObject, Time.time);
+        }
+        else if (currentDirection == Direction.Left)
+        {
+            animator.SetColFrameStart(2).SetRowFrameStart(1).SetTotalFrames(6).SetFramesPerSecond(5);
+            animator.Animate(gameObject, Time.time);
+        }
+    }
+
+    private void JumpAnimation()
+    {
+        if (currentDirection == Direction.Right)
+        {
+
+            animator.SetColFrameStart(3).SetRowFrameStart(0).SetTotalFrames(3).SetFramesPerSecond(3);
+            animator.Animate(gameObject, Time.time);
+        }
+        else if (currentDirection == Direction.Left)
+        {
+            animator.SetColFrameStart(3).SetRowFrameStart(1).SetTotalFrames(3).SetFramesPerSecond(3);
+            animator.Animate(gameObject, Time.time);
+        }
+    }
+
+    private void Controls()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            currentDirection = Direction.Left;
+            Debug.Log("Going left");
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            currentDirection = Direction.Right;
+            Debug.Log("Going right");
+        }
     }
 }
